76b8341623d19a5bcdf2b2b649a4d1f8e9c93fa5
sergiolevinmoov <sergio.levin@moovingtech.com>
2021-07-12 16:15:19 -0300

    Added Precision Recall Curve

--- ntl_xgboost.py
+++ ntl_xgboost.py
@@ -6,6 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score
 from sklearn.metrics import confusion_matrix
 
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import plot_precision_recall_curve
+import matplotlib.pyplot as plt
+
 data_path:str = 'C:/work/mooving/Predictive Analytics/NTL/xgboost/'
 training = pd.read_csv(data_path + 'trainingMDQ_ALL.csv')
 training.set_index('cuenta', inplace=True)
@@ -17,7 +21,7 @@ del training['fraude']
 del training['categoria']
 X_train, X_test, y_train, y_test = train_test_split(training, y, test_size=0.33, random_state=42)
 
-xg_reg = xgb.XGBRegressor(objective ='reg:logistic', scale_pos_weight=25, colsample_bytree=0.3, max_depth=5, alpha=10, n_estimators=10, enable_categorical=True)
+xg_reg = xgb.XGBClassifier(objective ='reg:logistic', scale_pos_weight=25, colsample_bytree=0.3, max_depth=5, alpha=10, n_estimators=10, enable_categorical=True)
 xg_reg.fit(X_train,y_train)
 preds = xg_reg.predict(X_test)
 predictions = [round(value) for value in preds]
@@ -29,3 +33,8 @@ print("Recall: %.2f%%" % (recall * 100.0))
 print("RMSE: %f" % (rmse))
 confusion_matrix = confusion_matrix(y_test, predictions)
 print(confusion_matrix)
+
+disp = plot_precision_recall_curve(xg_reg, X_test, y_test)
+disp.ax_.set_title('2-class Precision-Recall curve: ')
+
+plt.show()
